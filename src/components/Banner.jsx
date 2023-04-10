effe34df54ea9a83e0e500f971c04e5280e036ea
mosabbir.duet <mosabbir.duet@gmail.com>
2023-04-10 16:00:38 +0600

    Added banner section with responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>This is a banner area</h1>
+    <div className="mt-10 md:container mx-auto">
+      <div className="grid md:grid-cols-2 gap-10 items-center px-10 md:px-0">
+        <div>
+          <h1 className="text-5xl lg:text-6xl font-bold leading-tight">
+            One Step <br /> Closer To Your <br />
+            <span className="text-violet-500"> Dream Job</span>
+          </h1>
+          <p className="my-4">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
+          </p>
+          <button className="bg-gradient-to-br from-[#7E90FE] to-[#9873FF] px-6 md:px-5 py-3 rounded-lg text-white font-semibold text-xl  ">
+            Get Started
+          </button>
+        </div>
+        <img className="rounded-md" src="images/image.jpg" alt="" />
+      </div>
     </div>
   );
 };
